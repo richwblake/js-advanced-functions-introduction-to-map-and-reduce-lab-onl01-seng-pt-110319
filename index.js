c6401f98134ea1f0eb9bcffd5464465aa191bafd
Wills Blake <richwblake@gmail.com>
2020-07-04 00:49:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+function mapToNegativize (arr) {
+    let newArr = [];
+    arr.forEach(number => newArr.push(-number));
+    return newArr;
+}
+
+function mapToNoChange (arr) {
+    const newArr = arr;
+    return newArr;
+}
+
+function mapToDouble (arr) {
+    let newArr = [];
+    arr.forEach(number => newArr.push(number * 2));
+    return newArr;
+}
+
+function mapToSquare (arr) {
+    let newArr = [];
+    arr.forEach(number => newArr.push(number ** 2));
+    return newArr;
+}
+
+function reduceToTotal (arr, memo = 0) {
+    arr.forEach(number => memo += number);
+    return memo;
+}
+
+function reduceToAllTrue (src) {
+    for (let i = 0; i < src.length; i++ ) {
+        if (!src[i]) return false;
+      }
+    return true;
+}
+
+function reduceToAnyTrue (src) {
+    for (let i = 0; i < src.length; i++ ) {
+        if (src[i]) return true;
+      }
+    return false;
+}
+
